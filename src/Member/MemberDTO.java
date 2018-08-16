8aa1d15023d22538fc2bbea47a428a3592a0d4a3
kim <ilos1212@naver.com>
2018-08-16 18:48:04 +0900

    1

--- src/Member/MemberDTO.java
+++ src/Member/MemberDTO.java
@@ -1,11 +1,37 @@
 package Member;
 
+import java.util.Date;
+
 public class MemberDTO {
 	private int number;
 	private String id;
 	private String pw;
 	private String phonenum;
 	private String name;
+	private  Date left_time;
+	private int total_price;
+	public Date getLeft_time() {
+		return left_time;
+	}
+	public void setLeft_time(Date left_time) {
+		this.left_time = left_time;
+	}
+	public int getTotal_price() {
+		return total_price;
+	}
+	public MemberDTO(int number, String id, String pw, String phonenum, String name, Date left_time, int total_price) {
+		super();
+		this.number = number;
+		this.id = id;
+		this.pw = pw;
+		this.phonenum = phonenum;
+		this.name = name;
+		this.left_time = left_time;
+		this.total_price = total_price;
+	}
+	public void setTotal_price(int total_price) {
+		this.total_price = total_price;
+	}
 	public int getNumber() {
 		return number;
 	}
@@ -34,14 +60,6 @@ public class MemberDTO {
 		return name;
 	}
 	public void setName(String name) {
-		this.name = name;
-	}
-	public MemberDTO(int number, String id, String pw, String phonenum, String name) {
-		super();
-		this.number = number;
-		this.id = id;
-		this.pw = pw;
-		this.phonenum = phonenum;
 		this.name = name;
 	}
 	
