8923aaab950be1bc6c12ada80c9da959c46c711e
Yun Gun Hee <bear930920@gmail.com>
2018-08-20 09:18:43 +0900

    Merge

--- src/views/Client.java
+++ src/views/Client.java
@@ -182,7 +182,7 @@ public class Client extends JFrame implements ActionListener {
 		// 버튼 : 충 전, 사용 종료 , 닫기'
 		this.setVisible(true);
 //		192.168.0.84
-		String host="localhost";
+		String host="192.168.29.187";
 		int port=7777;
 		Socket socket=null;
 		PrintWriter pw=null;
